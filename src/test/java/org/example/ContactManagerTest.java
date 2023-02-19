10452c263f543af73076e34b128369c986040097
Sujith Krishan <sujithskrishna@gmail.com>
2023-02-19 18:36:33 +0000

    Updated Test

--- src/test/java/org/example/ContactManagerTest.java
+++ src/test/java/org/example/ContactManagerTest.java
@@ -1,23 +1,48 @@
 package org.example;
 
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 class ContactManagerTest {
+
+    private static ContactManager contactManager;
+
+    @BeforeAll
+    public static void initlize() {
+        System.out.println("Instantiating Contact Manager before the Test Execution");
+        contactManager = new ContactManager();
+    }
+
+    @BeforeEach
+    public void setup() {
+        System.out.println("Instantiating Contact Manager");
+        contactManager = new ContactManager();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.out.println("Should Execute After Each Test");
+    }
+
+    @AfterAll
+    public static void tearDownAll() {
+        System.out.println("Should be executed at the end of the Test");
+    }
+
     @Test
     @DisplayName("Should Create Contact")
+    @RepeatedTest(value = 5, name = "Repeating Contact Creation Test {currentRepetition} of {totalRepetitions}")
     public void shouldCreateContact() {
-        ContactManager contactManager = new ContactManager();
         contactManager.addContact("John", "Doe", "0123456789");
         assertFalse(contactManager.getAllContacts().isEmpty());
         assertEquals(1, contactManager.getAllContacts().size());
     }
+
+
     @Test
     @DisplayName("Should Throw Exception Contact")
     public void shouldThrowException() {
-        ContactManager contactManager = new ContactManager();
         contactManager.addContact("John", "Doe", "0123456789");
         Contact contact= new Contact("John", "Doe", "0123456789");
         Exception exception = assertThrows(RuntimeException.class, () -> {
@@ -26,9 +51,30 @@ class ContactManagerTest {
 
         String expectedMessage = "Contact Already Exists";
         String actualMessage = exception.getMessage();
-
         assertTrue(actualMessage.equals(expectedMessage));
+    }
 
+    @Test
+    @DisplayName("Should Not Create Contact When First Name is Null")
+    public void shouldThrowRuntimeExceptionWhenFirstNameIsNull() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact(null, "Doe", "0123456789");
+        });
+    }
 
+    @Test
+    @DisplayName("Should Not Create Contact When Last Name is Null")
+    public void shouldThrowRuntimeExceptionWhenLastNameIsNull() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact("John", null, "0123456789");
+        });
+    }
+
+    @Test
+    @DisplayName("Should Not Create Contact When Phone Number is Null")
+    public void shouldThrowRuntimeExceptionWhenPhoneNumberIsNull() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            contactManager.addContact("John", "Doe", null);
+        });
     }
 }
